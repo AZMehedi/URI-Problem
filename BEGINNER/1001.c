d96a2b4a8be80089b8042c26cd6e24ae1067676f
Mehedi Hasan <43952331+AZMehedi@users.noreply.github.com>
2018-10-30 13:04:21 +0600

    Add files via upload

---
+++ BEGINNER/1001.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x;
+    printf("Enter your number A\n");
+
+    scanf("%d",&b);
+    x=a+b;
+    printf("X = %d",x);
+    printf("\n");
+}
+
+
+#include<stdio.h>
+int main ()
+{
+    int a,b,x;
+    scanf("%d%d",&a,&b);
+    x=a+b;
+    printf("X = %d",x);
+    printf("\n");
+}
---
+++ BEGINNER/1009.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+char s;
+double a,b,TOTAL;
+scanf("%s%lf%lf",&s,&a,&b);
+TOTAL =a+((b*15)/100);
+printf("TOTAL = R$ %.2lf",TOTAL);
+printf("\n");
+return 0;
+}
